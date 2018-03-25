e8ac02ba651ad415e15e2fd5609c507bad428a07
José Cypriano Oliveira Júnior <jose_cypriano@icloud.com>
2018-03-25 03:51:25 +0100

    Update!

--- RScript_Udacity_Interview.R
+++ RScript_Udacity_Interview.R
@@ -1,26 +1,36 @@
-
 install.packages("dplyr")
 library(dplyr)
 
-
 #Upload Data Frames
 
-df_applications = read.table("https://raw.githubusercontent.com/josecypriano/Udacity_Interview/master/applications.csv",header = TRUE,sep=",")
-df_questions = read.table("https://raw.githubusercontent.com/josecypriano/Udacity_Interview/master/questions.csv",header = TRUE,sep=",")
+df_applications = read.table("https://raw.githubusercontent.com/josecypriano/Udacity_Interview/master/applications.csv",header = TRUE,sep=",",stringsAsFactors=FALSE)
+df_questions = read.table("https://raw.githubusercontent.com/josecypriano/Udacity_Interview/master/questions.csv",header = TRUE,sep=",",stringsAsFactors=FALSE)
 df_questionscategory = data.frame("Id_Question"=c(1,2,3), "Question2"=c("Education","Purpose","No Response"))
-
 
 df_questions_educ = subset(df_questions,question=="education")
 df_questions_educ$question = NULL
 
-df_applications
+
+#Treatment Data Analysis * Applications *
+
 df_applications$submitted_it = ifelse(df_applications$submitted_at=="","0","1")
 
+df_applications = left_join(df_applications,df_questions_educ %>% 
+                              select(applicant_id,response),
+                            by = c("applicant_id" = "applicant_id"))
 
-df_questions$Id_Question = ifelse(df_questions$response!="",df_questions$question,"3")
-df_questions = merge(x=df_questions,y=df_questionscategory,by="Id_Question",all.x = TRUE)
-df_questions$Id_Question = NULL
-  
+df_applications$response[df_applications$response==""] = "No Response"
+
+#Treatment Data Analysis * Questions *
+
+df_questions$question2 = ifelse(df_questions$response!="",df_questions$question,"No Response")
+df_questions2 = subset(df_questions,question2!="No Response")
+
+df_questions2 = table(df_questions2)
+
+df_questions2
+
+head(df_questions2)
 # Question 1: How Many Applicant submitted their application? How Many Didnt?
 
 table(df_applications$submitted_it)
@@ -33,11 +43,3 @@ Q1Plot = barplot(table(df_applications$submitted_it),
                  names.arg=c("No","Yes")
 )
 text(x = Q1Plot, y= table(df_applications$submitted_it), label = table(df_applications$submitted_it),pos = 3, xpd=NA)
-
-
-
-
-
-
-
-write.csv(df_applications,"dfitems.csv",row.names = FALSE)
